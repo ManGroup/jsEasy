336460bbb72aa13b312ea606e9319a62f2fda883
Antony <7923495@qq.com>
2014-11-30 22:14:24 +0800

    增加 Title

--- public/src/js/controllers/DetailController.js
+++ public/src/js/controllers/DetailController.js
@@ -4,20 +4,23 @@ jsEasyApp.controller('detailCtrl', ["$rootScope", "$scope" , 'app', '$location',
     var foodNum = 1;
 
     detailService.getDetail("1", function (err, data) {
+
+        $rootScope.title = data.data.name;
         $scope.file = data.data.fileId;
         $scope.title = data.data.name;
         $scope.price = data.data.price;
     });
+
     $scope.price = 0;
     $scope.foodNum = foodNum;
 
     $scope.addFood = function () {
         if (foodNum == 9)
             return;
-
         foodNum = foodNum + 1;
         $scope.foodNum = foodNum;
     };
+
     $scope.removeFood = function () {
         if (foodNum == 1)
             return;
