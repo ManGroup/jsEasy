316d78e05716c02ede7b29af771f3351db424477
chenyingxi <278305898@qq.com>
2014-12-01 22:00:53 +0800

    修复接口

--- public/src/js/controllers/IndexController.js
+++ public/src/js/controllers/IndexController.js
@@ -2,8 +2,7 @@
  * Created by Antony on 14/11/21.
  */
 
-jsEasyControllers.controller("indexCtrl", ["$scope", "$locale", 'foodService', 'app', function ($scope, $locale, foodService, app) {
-  console.log("this is index ctrl")
+jsEasyControllers.controller("indexCtrl", ["$scope", 'foodService', 'app', function ($scope, foodService, app) {
 
   foodService.foodlist(function (err, result) {
     $scope.foodlist = result.data;
