45d71d82320fbcbadc89ecdc66faeb978b91f07a
Antony <7923495@qq.com>
2014-12-01 20:35:41 +0800

    菜品列表画面 修改 ，增加响应式  样式

--- public/src/js/controllers/IndexController.js
+++ public/src/js/controllers/IndexController.js
@@ -2,12 +2,19 @@
  * Created by Antony on 14/11/21.
  */
 
-jsEasyControllers.controller("indexCtrl", ["$scope", "$locale", 'foodService', 'app', function ($scope, $locale, foodService, app) {
-  console.log("this is index ctrl")
+jsEasyControllers.controller("indexCtrl", ["$rootScope", "$scope", "$locale", 'foodService', 'app', '$location', function ($rootScope, $scope, $locale, foodService, app, $location) {
 
-  foodService.foodlist(function (err, result) {
-    $scope.foodlist = result.data;
-  });
+    $rootScope.title = "宅语菜园";
+
+    foodService.foodlist(function (err, result) {
+        $scope.foodlist = result.data;
+    });
+
+    $scope.gotoDetail = function () {
+        console.log("gotoDetail");
+        $location.search({}).path('/detail');
+    }
+
 
 }]);
 
