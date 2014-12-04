864262ed0e61dac345c1de5ffe778662dfcd4e3e
chenyingxi <278305898@qq.com>
2014-12-04 12:04:59 +0800

    下拉刷新第一版实现 当前仅能下拉刷新

--- public/src/js/controllers/IndexController.js
+++ public/src/js/controllers/IndexController.js
@@ -28,28 +28,42 @@ jsEasyControllers.controller('indexCtrl', ['$rootScope', '$scope', '$locale', 'f
 
   }
 
-}]);
-jsEasyControllers.directive('myScroll', ['$document', '$window', function ($document, $window) {
+}]).directive('myScroll', ['$document', '$window', 'foodService', function ($document, $window, foodService) {
 
   return function (scope, element, attr) {
 
-
-    var _offsetHeight = "123";
-    var _scrollTop = "123";
-
-
-    handler = function () {
+    /**
+     * 绑定scroll的执行事件
+     */
+    var handler = function () {
+      var eventLimit = 50;
+      var offsetHeight , scrollHeight , scrollTop;
       var elementBottom, remaining, shouldScroll, windowBottom;
 //      windowBottom = $window.height() + $window.scrollTop();
-      elementBottom = element.offset().top + element.height();
-      remaining = elementBottom - windowBottom;
-      shouldScroll = remaining <= $window.height() * scrollDistance;
+//      elementBottom = element.offset().top + element.height();
+//      remaining = elementBottom - windowBottom;
+//      shouldScroll = remaining <= $window.height() * scrollDistance;
+      offsetHeight = element[0].offsetHeight;
+      scrollHeight = element[0].scrollHeight;
+      scrollTop = element[0].scrollTop;
       console.info("**********************");
+      console.info("offsetHeight" + offsetHeight);
+      console.info("scrollHeight" + scrollHeight);
+      console.info("scrollTop" + scrollTop);
+      if (eventLimit > scrollHeight - scrollTop - offsetHeight) {
+        foodService.foodlist(function (err, result) {
+          console.info(result.data);
+          scope.foodlist.push(result.data);
+        });
+
+      }
+
 //      console.info("windowBottom"+windowBottom);
-      console.info("elementBottom" + elementBottom);
-      console.info("remaining" + remaining);
-      console.info("shouldScroll" + shouldScroll);
+//      console.info("elementBottom" + elementBottom);
+//      console.info("remaining" + remaining);
+//      console.info("shouldScroll" + shouldScroll);
     };
+
     element.on('scroll', handler);
   };
 }]);
